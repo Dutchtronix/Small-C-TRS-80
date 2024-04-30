5d13a3beadbf6e478dacb41c3a9ca696e33a37cd
JdR <jdr@dutchtronix.com>
2024-04-30 02:27:41 -0700

    Author info

--- cc0.c
+++ cc0.c
@@ -4,7 +4,7 @@
 **
 ** Copyright 1982 J. E. Hendrix
 **
-** Implemented TRS-80 Mod I Mikrorie - Jan de Rie
+** Implemented TRS-80 Mod I V2.1B Dutchtronix 2024
 **
 ** Global Data and Initialization Part
 */
